fc6de13ded753fb5e6219bc466280d140e297f9d
David Parton <david.parton@postmates.com>
2018-02-23 13:30:28 -0500

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pycernan",
-    version="0.0.1",
+    version="0.0.2",
     author="John Koenig",
     author_email="john@postmates.com",
     description="Python client for Cernan.",
