2165dfd115ea0531140bb571333abdaeecb7b7f7
Joe Astrauskas <joseph.astrauskas@postmates.com>
2018-04-09 17:25:17 -0700

    add deserialization and tests

--- tests/unit/avro/test_serde.py
+++ tests/unit/avro/test_serde.py
@@ -6,7 +6,7 @@ from avro.datafile import DataFileReader
 from io import BytesIO
 
 from pycernan.avro.exceptions import SchemaParseException, DatumTypeException
-from pycernan.avro.serde import parse, serialize
+from pycernan.avro.serde import parse, serialize, deserialize
 
 
 USER_SCHEMA = {
@@ -40,6 +40,20 @@ def test_serialize(ephemeral, schema):
         assert value is (b'1' if ephemeral else None)
         records = [r for r in reader]
         assert records == [user]
+
+
+def test_serialize_and_deserialize():
+    user = {
+        'name': 'Foo Bar Matic',
+        'favorite_number': 24,
+        'favorite_color': 'Nonyabusiness',
+    }
+
+    avro_blob = serialize(USER_SCHEMA, [user])
+    test_records = deserialize(avro_blob)
+    assert isinstance(test_records, list)
+    assert len(test_records) == 1
+    assert test_records[0] == user
 
 
 def test_bad_schema():
