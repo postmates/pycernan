aa144770574ea878cc0b283c122079907f25d8f0
Joseph Glanville <jpg@jpg.id.au>
2019-02-26 18:16:05 -0800

    XXX: Debug fastavro bug

--- tests/unit/avro/test_serde.py
+++ tests/unit/avro/test_serde.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from decimal import Decimal
 import pytest
 import types
@@ -9,6 +10,116 @@ from io import BytesIO
 from pycernan.avro.exceptions import SchemaParseException, SchemaResolutionException, DatumTypeException
 from pycernan.avro.serde import parse_schema, serialize, deserialize
 
+def datetime_to_epoch_micros(dt):
+    # https://stackoverflow.com/a/8778548
+    epoch = datetime(1970, 1, 1)
+    td = dt - epoch
+    return td.microseconds + (td.seconds + td.days * 24 * 3600) * 10 ** 6
+
+TIMESTAMP_MICROS_TYPE = {"type": "long", "logicalType": "timestamp-micros"}
+UUID_TYPE = {"type": "string", "logicalType": "uuid"}
+MAP_OF_STRINGS_TYPE = {"type": "map", "values": "string"}
+
+EVENT_METADATA_FIELDS = [
+    {"name": "event",           "type": "string"},
+    {"name": "object_type",     "type": "string"},
+    {"name": "object_uuid",     "type": ["long", UUID_TYPE]},
+    {"name": "one_stream_uuid", "type": UUID_TYPE},
+    {"name": "ts",              "type": TIMESTAMP_MICROS_TYPE},
+    {"name": "version",         "type": "int"}
+]
+
+SOUND_ENUM = {
+    "type": "enum",
+    "name": "sound",
+    "symbols": [
+        "alarm_multi.wav",
+        "beep_accept.wav",
+        "job_added.wav",
+        "job_removed.wav",
+        "off_duty_sound.aiff",
+        "order_updated.m4a",
+    ]
+}
+
+EVENT_DATA_RECORD = {
+    "type": "record",
+    "name": "event_data",
+    "fields": [
+        {"name": "alert",         "type": "string"},
+        {"name": "dispatch_uuid", "type": ["null", UUID_TYPE]},
+        {"name": "match_uuid",    "type": ["null", UUID_TYPE]},
+        {"name": "message",       "type": "string"},
+        {"name": "sound",         "type": ["null", SOUND_ENUM]},
+        {"name": "title",         "type": ["null", "string"]},
+    ]
+}
+
+EVENT_TYPE_ENUM = {
+    "type": "enum",
+    "name": "event_type",
+    "symbols": [
+        "account.sync",
+        "capabilities.vehicle",
+        "courier.force_offduty",
+        "courier.force_offduty_silent",
+        "courier_dispatch.canceled",
+        "courier_dispatch.new",
+        "job.admin_canceled",
+        "job.assigned_from",
+        "job.assigned_to",
+        "job.auto_assigned",
+        "job.customer_canceled",
+        "job.issue.updated",
+        "job.last_job",
+        "job.order.updated",
+        "job.pex_funds_added",
+        "job.pex_funds_not_added",
+        "job.reversed_to",
+        "message",
+        "verify.id",
+        "verify.id_and_over_21",
+        "verify.id_and_over_21_and_signature",
+        "verify.id_and_over_18",
+        "verify.id_and_over_18_and_signature",
+        "verify.id_and_signature",
+    ]
+}
+
+
+DATA_1_RECORD = {
+    "type": "record",
+    "name": "data_1",
+    "fields": [
+        {"name": "event_data",     "type": EVENT_DATA_RECORD},
+        {"name": "event_need_ack", "type": "boolean"},
+        {"name": "event_type",     "type": ["null", EVENT_TYPE_ENUM]},
+        {"name": "event_uuid",     "type": UUID_TYPE},
+    ]
+}
+
+DATA_2_RECORD = {
+    "type": "record",
+    "name": "data_2",
+    "fields": [
+        {"name": "message_data",         "type": MAP_OF_STRINGS_TYPE},  # todo: unconfirmed
+        {"name": "message_fetch_method", "type": "string"},  # todo: enum
+        {"name": "message_kind",         "type": "string"},  # todo: enum
+        {"name": "message_needs_ack",    "type": "boolean"},
+        {"name": "message_uuid",         "type": UUID_TYPE},
+    ]
+}
+
+# todo: event_created events seem to have two totally different signatures
+COURIER_EVENT_CREATED_EVENT_RECORD = {
+    "type": "record",
+    "namespace": "public.courier",
+    "name": "event_created",
+    "fields": [
+        {"name": "courier_uuid", "type": UUID_TYPE},
+        {"name": "data",         "type": [DATA_1_RECORD, DATA_2_RECORD]},
+    ] + EVENT_METADATA_FIELDS
+}
 
 USER_SCHEMA = {
     "namespace": "example.avro",
@@ -190,6 +301,31 @@ def test_logical_types():
     test_records = [value for value in test_generator]
     assert len(test_records) == 1
     assert test_records[0] == event
+
+
+def test_serde_malformed_input():
+    sample_courier_event_created_payload_1 = {
+        'courier_uuid': '0d6ce262-086c-4c15-90f9-a9448a696f8d',
+        'data': {
+            'event_data': {
+                'alert': 'Delivery Complete! You earned $4.77 before tip.',
+                'message': 'Delivery Complete! You earned $4.77 before tip.',
+                'sound': None,
+                'title': None
+            },
+            'event_need_ack': False,
+            'event_type': 'message',
+            'event_uuid': '81832ca5-5e53-4096-8868-58c41dbede79',
+        },
+        'event': 'event_created',
+        'object_type': 'courier',
+        'object_uuid': '0d6ce262-086c-4c15-90f9-a9448a696f8d',
+        'ts': datetime_to_epoch_micros(datetime.utcnow()),
+        'version': 2,
+        'one_stream_uuid': '09963f9f06fb477797f88f8b09530364'
+    }
+    serialize(COURIER_EVENT_CREATED_EVENT_RECORD, [sample_courier_event_created_payload_1])
+
 
 
 def test_serialize_and_deserialize_with_reader_schema():
